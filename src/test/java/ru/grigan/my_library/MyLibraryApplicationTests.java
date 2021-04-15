8fddae4596e72b6fc104e4f86fec2ff865930b69
grigan-Uragan <prischepny@mail.ru>
2021-04-15 23:04:35 +0300

    start project

---
+++ src/test/java/ru/grigan/my_library/MyLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.grigan.my_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
